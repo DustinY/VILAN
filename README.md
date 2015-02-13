6e9bc811d3475fce670144ea4bdda208cdd06799
Vicken52 <krikorian.vicken@gmail.com>
2015-02-12 20:44:29 -0800

    Update
    Dragonfly folder

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ### Installation
 
 1. Download Python: https://www.python.org/downloads/release/python-279/
-2. Download Python for Windows Extensions: http://sourceforge.net/projects/pywin32/files/pywin32/
+2. Download Python for Windows Extensions: http://sourceforge.net/projects/pywin32/files/pywin32/ 
+3. Move dragonfly Folder to C:\Python27\Lib\site-packages
--- VilanDragonfly.py
+++ VilanDragonfly.py
@@ -7,55 +7,62 @@ import time
 continueLoop = True
 #Class for creating the open rules
 grammar = Grammar("test grammar")
+from dragonfly.engines.backend_sapi5.engine import Sapi5InProcEngine
 
+engine = Sapi5InProcEngine()
+engine.connect()
 
 class TestRule(CompoundRule):
-    spec = "open <option>"
-    extras = [Choice("option", {
-        "chrome":"chrome",
-        "browser":"chrome",
-        "email":"www.gmail.com",
-        "the mail":"www.gmail.com",
-        "gmail":"www.gmail.com"
-        }
-        )
-        ]
-    def _process_recognition(self, node, extras):
-        chosen = extras["option"]
-        shell = Key("w-r")
-        shell.execute()
-        shell = Text(chosen)
-        shell.execute()
-        shell = Key("enter")
-        shell.execute()
+	spec = "open <option>"
+	extras = [Choice("option", {
+		"chrome":"chrome",
+		"browser":"chrome",
+		"email":"www.gmail.com",
+		"the mail":"www.gmail.com",
+		"gmail":"www.gmail.com"
+		}
+		)
+		]
+	def _process_recognition(self, node, extras):
+		chosen = extras["option"]
+		shell = Key("w-r")
+		shell.execute()
+		time.sleep(.1)
+		shell = Text(chosen)
+		shell.execute()
+		shell = Key("enter")
+		shell.execute()
 
 rule = TestRule()
 grammar.add_rule(rule)
 
 
 class TurnoffRule(CompoundRule):
-    spec = "turn <option>"
-    extras = [Choice("option", {
-        "off":"off",
-        "on":"on"}
-        )
-        ]
-    def _process_recognition(self, node, extras):
-        chosen = extras["option"]
-        if chosen == "off":
-            self.loopStatus = "off"
+	spec = "turn <option>"
+	extras = [Choice("option", {
+		"off":"off",
+		"on":"on"}
+		)
+		]
+	def _process_recognition(self, node, extras):
+		chosen = extras["option"]
+		if chosen == "off":
+			self.loopStatus = "off"
 
 
 rule = TurnoffRule()
 grammar.add_rule(rule)
-grammar.load()
-
+#grammar.load()
+engine._load_grammar(grammar)
+#engine.activate_grammar(grammar)
 
 #def response(phrase, listenter):
-#    print "You said %s" % phrase
+#	print "You said %s" % phrase
 
 
 while continueLoop:
-    pythoncom.PumpWaitingMessages()
-    print "Waiting"
-    time.sleep(.1)
+
+	pythoncom.PumpWaitingMessages()
+	print engine._recognizer_dispatch_name
+	print "Waiting"
+	time.sleep(.1)

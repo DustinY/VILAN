8ef83f80f0c1fcd506fb2b41bc9d39f849ef3199
dsyed <dsyed@calstatela.edu>
2015-02-12 22:58:52 -0800

    Merge previous changes, Add gitignore file
    Merged Dustin's last upload with the main folder, and added a .gitignore
    file to keep things clean.

--- fileiotest.py
+++ fileiotest.py
@@ -1,6 +1,3 @@
-# if input starts with some p from phrases, 
-# (input - p) is passed as arg to p's function
-
 import keywordmodules as km
 
 def read_keywords(filename):
@@ -14,10 +11,31 @@ def read_keywords(filename):
 
 	return keywords
 
-keywords = read_keywords('keywords.dat')
+def exec_input(input):
+	input = input.lower()
+	recognized = False
 	
-input = "open chrome"
-for k in keywords:
-	if input.startswith(k):
-		arg = input.replace(k,"").lstrip()
-		eval("km." + k + "(\"" + arg + "\")")
+	for k in keywords:
+		if input.startswith(k):
+			arg = input.replace(k,"").lstrip()
+			k = k.replace(" ","_")
+			eval("km." + k + "(\"" + arg + "\")")
+			recognized = True
+			break
+	
+	if not recognized:
+		print "I don't understand:", input
+
+keywords = read_keywords('keywords.dat')
+
+
+# Tests
+inputs = []
+inputs.append("open chrome")
+inputs.append("open notchrome")
+inputs.append("go to google")
+inputs.append("go to notgoogle")
+inputs.append("fakecommand test")
+
+for input in inputs:
+	exec_input(input)

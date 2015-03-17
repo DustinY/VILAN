85a7772f90a4ab1573593e8add27e3a378da3f45
dsyed <dsyed@calstatela.edu>
2015-03-17 15:47:25 -0700

    Fix some pep8 complaints

--- customGrammar.py
+++ customGrammar.py
@@ -4,50 +4,54 @@ import win32com.client
 import time
 from vilanGrammars import *
 
-import os.path, time
+import os.path
+import time
 import threading
 
+
 def create_dict_from_file(filename):
-	dict = {}
-	f = open(filename)
+    dict = {}
+    f = open(filename)
 
-	for line in f:
-		s = line.rstrip()
-		if s and not s.startswith("#"):
-			key, value = s.split(",")
-			dict[key] = value
+    for line in f:
+        s = line.rstrip()
+        if s and not s.startswith("#"):
+            key, value = s.split(",")
+            dict[key] = value
 
-	return dict
+    return dict
 
 datelastMod = time.ctime(os.path.getmtime("customCommands.dat"))
 customCommands = create_dict_from_file("customCommands.dat")
 
+
 class mainRule(CompoundRule):
-	spec = "open <option>"
-	extras = [Choice("option", customCommands)]
+    spec = "open <option>"
+    extras = [Choice("option", customCommands)]
 
-	def _process_recognition(self, node, extras):
-		chosen = extras["option"]
+    def _process_recognition(self, node, extras):
+        chosen = extras["option"]
 
-		shell = Key("w-r")
-		shell.execute()
-		time.sleep(.1)
-		shell = Text(chosen)
-		shell.execute()
-		time.sleep(.1)
-		shell = Key("enter")
-		shell.execute()
+        shell = Key("w-r")
+        shell.execute()
+        time.sleep(.1)
+        shell = Text(chosen)
+        shell.execute()
+        time.sleep(.1)
+        shell = Key("enter")
+        shell.execute()
 
 rule = mainRule()
 customGrammar.add_rule(rule)
 
+
 def lastMod():
     newDateLastMod = time.ctime(os.path.getmtime("customCommands.dat"))
 
-	if newDateLastMod is not dateLastMod:
-		customCommands = create_dict_from_file("customCommands.dat")
-		rule = mainRule()
-		customGrammar.add_rule(rule)
+    if newDateLastMod is not dateLastMod:
+        customCommands = create_dict_from_file("customCommands.dat")
+        rule = mainRule()
+        customGrammar.add_rule(rule)
 
     threading.Timer(5, lastMod).start()
 
--- dateModTest.py
+++ dateModTest.py
@@ -1,8 +1,12 @@
-import os.path, time
+import os.path
+import time
 import threading
 
+
 def lastMod():
-    print "last modified: %s" % time.ctime(os.path.getmtime("dateModTestFile.txt"))
+    print "last modified: %s" %
+    time.ctime(os.path.getmtime("dateModTestFile.txt"))
+
     threading.Timer(5, lastMod).start()
 
 lastMod()
